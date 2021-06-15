265b5d8c5388f79008ed36ebd0649b28db63844a
Anthony DeCapite <anthony.decapite@icloud.com>
2021-06-15 10:58:43 -0400

    making error codes.

--- public/db.js
+++ public/db.js
@@ -5,3 +5,14 @@ request.onupgradeneeded = function (event) {
     const db = event.target.result;
     db.createObjectStore("pending", { autoIncrement: true });
 };
+
+request.onsuccess = function (event){
+    db = event.target.result;
+    if (navigator.onLine){
+        checkDatabase();
+    }
+};
+
+request.onerror = function (event){
+    console.log("error" + event.target.errorCode);
+};
